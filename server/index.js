4e08c9011bd146e6968329be45406b334b7df1fa
Clarence-Lam <475603284@qq.com>
2018-09-24 00:28:58 +0800

    增加模版引擎中间件

--- server/index.js
+++ server/index.js
@@ -1,6 +1,15 @@
 const Koa = require ('koa')
 const app = new Koa();
+const views =require('koa-views')
+const { resolve } = require('path')
+
+app.use(views(resolve(__dirname, './views'), {
+    extension: 'pug'
+}))
 app.use(async (ctx, next) => {
-    ctx.body = 'koa'
+    await ctx.render('index',{
+        you:'luke',
+        me:'clarecne'
+    })
 })
 app.listen(4455)
---
+++ server/tpl/ejs.js
@@ -0,0 +1,27 @@
+module.exports = `
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <meta charset="utf-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+            <title>实例</title>
+            <link href="https://cdn.bootcss.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" rel="stylesheet">
+            <script src="https://cdn.bootcss.com/jquery/3.2.0/jquery.min.js"></script>
+            <script src="https://cdn.bootcss.com/bootstrap/4.0.0-beta.2/js/bootstrap.bundle.min.js"></script>
+        </head>
+        <body>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8">
+                        <h1>hi <%= you %></h1>
+                        <p><%= me %></p>
+                    </div>
+                    <div class="col-md-4">
+                        测试
+                    </div>
+                </div>
+            </div>        
+        </body>
+    </html>
+
+`
